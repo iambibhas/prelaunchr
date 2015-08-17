10cd3b9aa9b69a3b4187be937c17dd4f8c99465b
Bibhas <iambibhas@gmail.com>
2015-08-17 23:06:35 +0530

    added smtp config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,18 @@ Prelaunchr::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.action_mailer.default_url_options = { :host => 'www.example.com' }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { host: "garagely.in" }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  address:              'smtp.gmail.com',
+  port:                 587,
+  domain:               'garagely.in',
+  user_name:            ENV['SMTP_USERNAME'],
+  password:             ENV['SMTP_PASSWORD'],
+  authentication:       'plain',
+  enable_starttls_auto: true  }
 
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
