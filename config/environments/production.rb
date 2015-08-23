37d4eadd2d0cf14872c837f19a2c36d33dec132b
Bibhas <iambibhas@gmail.com>
2015-08-23 18:36:58 +0530

    changed default host to www

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Prelaunchr::Application.configure do
 
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: "garagely.in" }
+  config.action_mailer.default_url_options = { host: "www.garagely.in" }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
   address:              'smtp.gmail.com',
